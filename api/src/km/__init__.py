0b610d34cd7428941bcce6bc92f630f76190de3a
Christian Dein <christian.dein@dein-hosting.de>
2021-04-08 18:27:29 +0200

    Inital api server setup

---
+++ api/src/km/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.update(test_config)
+
+    @app.route('/')
+    def root():
+        return jsonify({'version': '0.1'})
+
+    return app
---
+++ api/tests/test_create_app.py
@@ -0,0 +1,12 @@
+from km import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_root_api(client):
+    response = client.get('/')
+    assert response.headers.get('content-type') == 'application/json'
+    assert response.data == b'{"version":"0.1"}\n'
